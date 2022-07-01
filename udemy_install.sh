c79d76041ef7fc5b87a3478ac68a11031251863a
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2022-07-01 08:29:41 +0530

    bash Script for termux user

---
+++ udemy_install.sh
@@ -0,0 +1,11 @@
+pkg install wget -y
+pkg install python3 -y
+pkg install git -y 
+git clone https://github.com/AKhilRaghav0/udemy-enroller/ #cloaning repo
+cd udemy-enroller
+ls
+python3 -m pip install --upgrade pip 
+pip3 install -r requirments.txt
+pip3 install wheel
+python setup.py install 
+udemy_installer 
